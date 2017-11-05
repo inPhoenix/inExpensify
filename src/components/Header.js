1da1947a1004b9678eaf1d654bfab5ef98b4e215
inPhoenix <spiderk@gmail.com>
2017-11-05 12:20:40 +0000

    :sparkles: Adding Expense DashBoard Page

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,9 @@ import { NavLink } from 'react-router-dom'
 const Header = () => (
   <header>
     <h1>inExpensify</h1>
-    <NavLink to='/' activeClassName='is-active' exact>Home</NavLink>
-    <NavLink to='/help' activeClassName='is-active'> Help</NavLink>
-    <NavLink to='/edit' activeClassName='is-active'> Edit</NavLink>
-    <NavLink to='/create' activeClassName='is-active'> Create</NavLink>
+    <NavLink className='temp' to='/' activeClassName='is-active' exact>Dashboard</NavLink>
+    <NavLink className='temp' to='/create' activeClassName='is-active'> Create</NavLink>
+    <NavLink className='temp' to='/help' activeClassName='is-active'> Help</NavLink>
   </header>
 )
 export default Header
