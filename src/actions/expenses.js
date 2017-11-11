5242db3423fbb79855a3e49825447be7bf1718ee
inPhoenix <spiderk@gmail.com>
2017-11-11 15:00:42 +0000

    :boom: Adding removal logic from firebase

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,4 +1,4 @@
-import uuid from 'uuid';
+//import uuid from 'uuid';
 import database from '../firebase/firebase'
 
 // ADD_EXPENSE
@@ -34,6 +34,14 @@ export const removeExpense = ({ id } = {}) => ({
   type: 'REMOVE_EXPENSE',
   id
 });
+
+export const startRemoveExpense = ({ id } = {}) => {
+  return (dispatch) => {
+    return database.ref(`expenses/${id}`).remove().then(() => {
+      dispatch(removeExpense({ id }))
+    })
+  }
+}
 
 // EDIT_EXPENSE
 export const editExpense = (id, updates) => ({
--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,31 +1,37 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import ExpenseForm from './ExpenseForm'
-import { editExpense, removeExpense } from '../actions/expenses'
+import {
+  editExpense,
+  startRemoveExpense
+ // removeExpense
+} from '../actions/expenses'
 
 
 //Editing the expense with id of {props.match.params.id}
-const EditExpensePage = (props) => {
-  console.log('props Higher::: ', props )
-  return (
-    <div>
-      id:
-      {props.match.params.id}
-      <ExpenseForm
-        expense={props.expense} // magic line that will update
-        onSubmit={(expense) => {
-          props.dispatch(editExpense(props.expense.id, expense))
-          props.history.push('/')
-        }}
-      />
-      <button
-        onClick={() => {
-          props.dispatch(removeExpense({ id: props.expense.id } ))
-          props.history.push('/')
-        }}
-      >Remove </button>
-    </div>
-  )
+export class EditExpensePage extends React.Component  {
+  onSubmit = (expense) => {
+    this.props.editExpense(this.props.expense.id, expense)
+    this.props.history.push('/')
+  }
+  onRemove = () => {
+    this.props.startRemoveExpense({ id: this.props.expense.id })
+    this.props.history.push('/')
+  }
+
+  render() {
+    return (
+      <div>
+        id:
+        {this.props.match.params.id}
+        <ExpenseForm
+          expense={this.props.expense} // magic line that will update
+          onSubmit={this.onSubmit}
+        />
+        <button onClick={this.onRemove}>Remove </button>
+      </div>
+    )
+  }
 }
 
 const mapStateToProps = (state, props) => {
@@ -34,12 +40,10 @@ const mapStateToProps = (state, props) => {
   };
 };
 
-/*const mapStateToProps = (state, props) => {
-  // console.log('props ', props )
-  console.log('state ', state )
-  return {
-    expense: state.expenses.find((expense) => expense.id === props.match.params.id)
-  }
-}*/
+const mapDispatchToProps = (dispatch, props) => ({
+  editExpense: (id, expense) => dispatch(editExpense(id, expense)),
+  startRemoveExpense: (data) => dispatch(startRemoveExpense(data))
+})
 
-export default connect(mapStateToProps)(EditExpensePage)
+
+export default connect(mapStateToProps, mapDispatchToProps)(EditExpensePage)
